88cdd253ee712e746617d1af584355049630d6f6
BellachhebKhalid <kd.bellachheb@gmail.com>
2021-06-02 15:04:44 +0000

    acces to uplink data with api

--- api/views.py
+++ api/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 @csrf_exempt 
 def save_data(request):
     if (True):
         if request.method=='POST':
-            print(request.body)
-    return HttpResponse(ok)
+            data = json.loads(request.body.decode('utf-8'))
+            print('Autorisation ': data["uplink_message"]["decoded_payload"]["Autorisation"])
+    return HttpResponse("ok")
