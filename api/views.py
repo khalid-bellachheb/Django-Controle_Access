762025e8fda776e21fc7646bf5f0a07db0e8c8ed
BellachhebKhalid <kd.bellachheb@gmail.com>
2021-06-02 15:14:09 +0000

    uplink data from ttn api/views.py

--- api/views.py
+++ api/views.py
@@ -9,5 +9,8 @@ def save_data(request):
     if (True):
         if request.method=='POST':
             data = json.loads(request.body.decode('utf-8'))
-            print('Autorisation ': data["uplink_message"]["decoded_payload"]["Autorisation"])
+            print('Autorisation : ', data["uplink_message"]["decoded_payload"]["Autorisation"])
+            print('Badge : ', data["uplink_message"]["decoded_payload"]["Badge"])
+            print('Porte :  ', data["uplink_message"]["decoded_payload"]["Porte"])
+            print('Zone :  ', data["uplink_message"]["decoded_payload"]["Zone"])
     return HttpResponse("ok")
