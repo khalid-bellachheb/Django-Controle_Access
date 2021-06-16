a0aaff3126efea6eaa35f8327809ba95bb2157aa
khalid <k8bellac@enib.fr>
2021-06-16 15:10:52 +0200

    save data from ttn

--- api/views.py
+++ api/views.py
@@ -4,17 +4,57 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from data_ttn.models import data_ttn
 
+def decrypte_Autorisation(auto):
+    if (auto == "1"):
+        return "Accés autorisé"
+    if (auto == "0"):
+        return 'Accés non autorisé'
+    return ''
 
+def decrypte_Badge(bad):
+    return 
+
+def decrypte_Porte(por):
+    if (por == "A"):
+        return "A"
+    if (por == "B"):
+        return "B"
+    if (por == "C"):
+        return "C"
+    if (por == "D"):
+        return "D"
+    if (por == "E"):
+        return "E"
+    return ""
+
+def decrypte_Zone(zon):
+    if (zon == "1"):
+        return "Zone 1"
+    if (zon == "2"):
+        return "Zone 2"
+    if (zon == "3"):
+        return "Zone 3"
+    return ""
+
+def decrypte_status(sta):
+    if (sta == "E"):
+        return "Employé(e)"
+    if (sta == "V"):
+        return "Visiteur"
+    return ""
 # Create your views here.
 # api/up
 @csrf_exempt 
 def save_data(request):
     if request.method=='POST':
         data = json.loads(request.body.decode('utf-8'))
-        print('Autorisation : ', data["uplink_message"]["decoded_payload"]["Autorisation"])
-        print('Badge : ', data["uplink_message"]["decoded_payload"]["Badge"])
-        print('Porte :  ', data["uplink_message"]["decoded_payload"]["Porte"])
-        print('Zone :  ', data["uplink_message"]["decoded_payload"]["Zone"])
+        auto=decrypte_Autorisation(data["uplink_message"]["decoded_payload"]["Autorisation"])
+        bad=data["uplink_message"]["decoded_payload"]["Badge"]
+        por=decrypte_Porte(data["uplink_message"]["decoded_payload"]["Porte"])
+        zon=decrypte_Zone(data["uplink_message"]["decoded_payload"]["Zone"])
+        sta=decrypte_status(data["uplink_message"]["decoded_payload"]["status"])
+        obj=data_ttn(badge=bad,Autorisation=auto,Porte=por, Zone=zon,status=sta)
+        obj.save()
         
     return HttpResponse("ok")
 
@@ -42,6 +82,13 @@ def save_faceRecognition_data(request):
         print("face Recognition id was send")
         data = json.loads(request.body.decode('utf-8'))
         data = json.loads(data)
+        d=data_ttn.objects.filter(badge=data["Badge"])
+        if d :
+            for dd in d :
+                dd.matricule_id=data["Nom"]
+                dd.save()
+        else:
+            print("send right data")
         print(data)
         
     return HttpResponse("ok")
--- send_data_ttn/sendingTest.py
+++ send_data_ttn/sendingTest.py
@@ -1,18 +1,40 @@
-import requests,time
-
+import requests,json
+'''
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': '',
+    'Authorization': 'Bearer HG26O4CZYQFL7QQZ35ITSB6STD733Q3BGZ7VDNA',
+}
+
+
+
+data = {
+
+  "webhook": {},
+  "field_mask": {},
+
+
+}
+
+d=json.dumps(data)
+
+
+r = requests.post('https://eu1.cloud.thethings.network/api/v3/as/webhooks/http-endpoint.stm32-1', headers=headers, data=data)
+
+print(r.status_code)
+print (r.content)'''
+
+headers = {
+    'Authorization': 'Bearer NNSXS.HG26O4CZYQFL7QQZ35ITSB6STD733Q3BGZ7VDNA',
+    'Content-Type': 'application/json',
+    'User-Agent': 'my-integration/my-integration-version',
 }
 
 data = {
-  '{ "dev_id": "stm32-1","port": 2,"confirmed": false,"payload_raw": {"led": 1} }'
+  '{ "downlinks": [{ "frm_payload": "vu8': '", "f_port": 42, }] }'
 }
 
+response = requests.post('https://eu1.cloud.thethings.network/api/v3/as/applications/pre-2021-lora-rfid/devices/stm32-1/down/push', headers=headers, data=data)
 
-data =({"ts":time.time()})
 
-r = requests.post('https://eu1.cloud.thethings.network/api/v3/as/applications/pre-2021-lora-rfid/webhooks/http-endpoint/devices/stm32-1/down/push', headers=headers, data=data)
-
-print(r.status_code)
-print (r.content)
+print(response.status_code)
+print (response.content)
